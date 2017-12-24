d8c41a1bee2ff4b6ff2d98d3450f3813c60abb55
Dmitriy00047 <dima.kibetc@gmail.com>
2017-12-24 22:44:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+app.listen(3000);
